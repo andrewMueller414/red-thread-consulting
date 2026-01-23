b81233a059eda3fdd2dd7e4600259a6cb6ac41a7
Uh Little Less Dum <130193337+Uh-little-less-dum@users.noreply.github.com>
2026-01-22 19:47:48 -0600

    Add image priority to avoid layout shift

--- features/onboarding/onboarding_form/onboarding_form_section.tsx
+++ features/onboarding/onboarding_form/onboarding_form_section.tsx
@@ -1,58 +1,58 @@
 import React, { type ReactNode } from "react";
 import {
-  OnboardingFormSectionItem,
-  OnboardingFormType,
+    OnboardingFormSectionItem,
+    OnboardingFormType,
 } from "../onboarding_types";
 import { OnboardingSectionImage } from "./onboarding_section_image";
 import { OnboardingSectionForm } from "./onboarding_section_form";
 import { cn } from "@/lib/utils";
 
 interface OnboardingFormSectionProps {
-  item: OnboardingFormSectionItem;
-  idx: number;
-  form: OnboardingFormType;
+    item: OnboardingFormSectionItem;
+    idx: number;
+    form: OnboardingFormType;
 }
 
 const SectionContainer = ({
-  children,
-  className,
+    children,
+    className,
 }: {
-  className?: string;
-  children: ReactNode;
+    className?: string;
+    children: ReactNode;
 }): ReactNode => {
-  return (
-    <div
-      className={cn(
-        "w-full max-w-270 flex flex-col lg:grid lg:grid-cols-2 px-8 md:px-12 my-8 gap-x-6",
-        className,
-      )}
-    >
-      {children}
-    </div>
-  );
+    return (
+        <div
+            className={cn(
+                "w-full max-w-270 flex flex-col lg:grid lg:grid-cols-2 px-8 md:px-12 my-8 gap-x-6",
+                className,
+            )}
+        >
+            {children}
+        </div>
+    );
 };
 
 export const OnboardingFormSection = ({
-  item,
-  idx,
-  form,
+    item,
+    idx,
+    form,
 }: OnboardingFormSectionProps): ReactNode => {
-  const imageLeft = idx % 2 === 0;
-  if (imageLeft) {
-    return (
-      <SectionContainer>
-        <OnboardingSectionImage image={item.image} />
-        <OnboardingSectionForm form={form} item={item} float={"right"} />
-      </SectionContainer>
-    );
-  } else {
-    return (
-      <SectionContainer className="flex-col-reverse">
-        <OnboardingSectionForm form={form} item={item} float={"left"} />
-        <OnboardingSectionImage image={item.image} />
-      </SectionContainer>
-    );
-  }
+    const imageLeft = idx % 2 === 0;
+    if (imageLeft) {
+        return (
+            <SectionContainer>
+                <OnboardingSectionImage priority={idx <= 2} image={item.image} />
+                <OnboardingSectionForm form={form} item={item} float={"right"} />
+            </SectionContainer>
+        );
+    } else {
+        return (
+            <SectionContainer className="flex-col-reverse">
+                <OnboardingSectionForm form={form} item={item} float={"left"} />
+                <OnboardingSectionImage priority={idx <= 2} image={item.image} />
+            </SectionContainer>
+        );
+    }
 };
 
 OnboardingFormSection.displayName = "OnboardingFormSection";
--- features/onboarding/onboarding_form/onboarding_section_image.tsx
+++ features/onboarding/onboarding_form/onboarding_section_image.tsx
@@ -4,10 +4,12 @@ import { OnboardingFormSectionItem } from "../onboarding_types";
 
 interface OnboardingSectionImageProps {
     image: OnboardingFormSectionItem["image"];
+    priority?: boolean;
 }
 
 export const OnboardingSectionImage = ({
     image,
+    priority = false,
 }: OnboardingSectionImageProps): ReactNode => {
     return (
         <div className="max-h-80 lg:max-h-full lg:w-full h-full overflow-hidden flex flex-col justify-center items-center">
@@ -18,6 +20,8 @@ export const OnboardingSectionImage = ({
                 width={1080}
                 height={1080}
                 className="max-w-[80%] max-h-80 lg:max-h-full object-contain opacity-80"
+                priority={priority}
+                loading={priority ? "eager" : "lazy"}
             />
         </div>
     );
