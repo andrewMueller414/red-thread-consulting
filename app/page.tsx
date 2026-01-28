178ba0a33a5f19a9b53ea55935babb3f915b3ee7
Uh Little Less Dum <130193337+Uh-little-less-dum@users.noreply.github.com>
2026-01-28 17:52:40 -0600

    Clean up landing page now that we're moving to an mdx based approach and I'm not sure what will even go there...

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { NoneFoundView } from "@/core/shared_components/none_found_view";
+
 export default function Home() {
     return (
-        <div>
-            Pretty much all of the functionality exists in the admin page for now.
+        <div className="w-full min-h-screen flex flex-col justify-center items-center">
+            <NoneFoundView
+                label="Nothing to show here..."
+                body="Moved pretty much all of the functionality to the admin page for now."
+            />
         </div>
     );
 }
